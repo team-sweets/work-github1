11882423f6fcf3c1753cac62b4da264e7aabe7b4
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-16 08:04:58 +0000

    whole new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 会員側のルーティング設定
+  # devise_for :customers
+  # root
+  
+  # 管理者側のルーティング設定
+  namespace :admin do
+    get 'items' => 'admin/items#index'
+  end
 end
