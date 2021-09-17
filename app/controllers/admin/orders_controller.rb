3532940f329b20728e92cdd542432ef65a538794
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-17 06:07:15 +0000

    controllers generated

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,10 @@
+class Admin::OrdersController < ApplicationController
+  def top
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
---
+++ db/migrate/20210917052253_change_column_to_customer.rb
@@ -0,0 +1,32 @@
+class ChangeColumnToCustomer < ActiveRecord::Migration[5.2]
+  def change
+  #   # 変更内容
+  # def up
+  #   # change_column :customers, :, :string, null: false, default: 0
+    
+  #   change_column :customers, :first_name, :string, null: false
+  #   change_column :customers, :last_name, :string, null: false
+  #   change_column :customers, :kana_first_name, :string, null: false
+  #   change_column :customers, :kana_last_name, :string, null: false
+  #   change_column :customers, :postal_code, :string, null: false
+  #   change_column :customers, :address, :string, null: false
+  #   change_column :customers, :phone_number, :string, null: false
+  #   change_column :customers, :is_deleted, :boolean, null: false, default: FALSE
+  # end
+
+  # # 変更前の状態
+  # def down
+  #   # change_column :users, :uuid, :string, null: true, default: 0
+    
+  #   change_column :customers, :first_name, :string, false
+  #   change_column :customers, :last_name, :string, false
+  #   change_column :customers, :kana_first_name, :string, false
+  #   change_column :customers, :kana_last_name, :string, false
+  #   change_column :customers, :postal_code, :string, false
+  #   change_column :customers, :address, :string, false
+  #   change_column :customers, :phone_number, :string, false
+  #   change_column :customers, :is_deleted, :boolean, false
+    
+  # end
+  # end
+end
