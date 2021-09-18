93b9c2b8b5c6ee87d2617e5f8bdc3d1fd8ae88d4
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-18 06:56:52 +0000

    customers model column-changed

--- db/migrate/20210917045231_add_columns_to_customers.rb
+++ db/migrate/20210917045231_add_columns_to_customers.rb
@@ -1,12 +1,12 @@
 class AddColumnsToCustomers < ActiveRecord::Migration[5.2]
   def change
-    add_column :customers, :first_name, :string, false
-    add_column :customers, :last_name, :string, false
-    add_column :customers, :kana_first_name, :string, false
-    add_column :customers, :kana_last_name, :string, false
-    add_column :customers, :postal_code, :string, false
-    add_column :customers, :address, :string, false
-    add_column :customers, :phone_number, :string, false
-    add_column :customers, :is_deleted, :boolean, false
+    add_column :customers, :first_name, :string
+    add_column :customers, :last_name, :string
+    add_column :customers, :kana_first_name, :string
+    add_column :customers, :kana_last_name, :string
+    add_column :customers, :postal_code, :string
+    add_column :customers, :address, :string
+    add_column :customers, :phone_number, :string
+    add_column :customers, :is_deleted, :boolean
   end
 end
