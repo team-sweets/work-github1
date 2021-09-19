5d30cd8fad52e8f4114a2cff0ff45b3aab52095c
EC2 Default User <ec2-user@ip-172-31-14-149.ec2.internal>
2021-09-19 14:23:58 +0000

    admin-sign-in

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
+<<<<<<< HEAD
 
   attachment :image
 
   enum sales_status: { on_sale: 0, sold_out: 1 }
 
+=======
+  belongs_to :genre
+>>>>>>> origin/develop
 end
