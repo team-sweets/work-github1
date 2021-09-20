336115a4f43d62a4c34b65eb33ea9cfba3ef4585
EC2 Default User <ec2-user@ip-172-31-88-199.ec2.internal>
2021-09-20 13:36:23 +0000

    products index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,16 @@
 class Product < ApplicationRecord
  attachment :image
 
- enum sales_status: { on_sale: 0, sold_out: 1 }
+ #enum sales_status: { on_sale: 0, sold_out: 1 }
+ validates :sales_status, inclusion: { in: [true, false] }
 
  belongs_to :genre
-  
+
  has_many :cart_items
-  
+
  has_many :order_details
+
+  def add_tax_tax_out_price
+    (self.price * 1.10).round
+  end
 end
