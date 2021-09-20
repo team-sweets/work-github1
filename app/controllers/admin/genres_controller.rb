699e12872f44a81daf46b485426d8a48758a42a5
EC2 Default User <ec2-user@ip-172-31-25-157.ec2.internal>
2021-09-20 04:26:43 +0000

    tyuukann-table

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,5 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
@@ -21,7 +21,7 @@ class Admin::GenresController < ApplicationController
     genre.update(genre_params)
     redirect_to admin_genres_path
   end
-  
+
   private
   def genre_params
     params.require(:genre).permit(:name)
