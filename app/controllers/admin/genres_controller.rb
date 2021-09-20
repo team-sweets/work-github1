91847cafc3097227b29454a7477c6a5826ab1116
EC2 Default User <ec2-user@ip-172-31-14-149.ec2.internal>
2021-09-20 07:10:01 +0000

    marging

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,5 @@
 class Admin::GenresController < ApplicationController
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> origin/develop
   def index
     @genre = Genre.new
     @genres = Genre.all
@@ -25,11 +21,7 @@ class Admin::GenresController < ApplicationController
     genre.update(genre_params)
     redirect_to admin_genres_path
   end
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> origin/develop
   private
   def genre_params
     params.require(:genre).permit(:name)
