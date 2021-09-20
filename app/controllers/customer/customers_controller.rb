af411a808562bb3d4935253544497c0d294cb415
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-20 13:37:33 +0000

    Temporarily_saved

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -14,10 +14,17 @@ class Customer::CustomersController < ApplicationController
   end
 
   def update
+      @cusomer = current_customer
+    if @cusomer.update(customer_params)
+      redirect_to customers_path
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   private
   def customer_params
-    params.require(:cusomer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number)
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number)
   end
 end
