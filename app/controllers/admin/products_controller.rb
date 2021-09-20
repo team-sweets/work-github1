60f037af6e928943deeae547717f2efe1774d461
EC2 Default User <ec2-user@ip-172-31-88-199.ec2.internal>
2021-09-20 06:26:56 +0000

    bootstrap

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
   def index
+
   end
 
   def new
@@ -9,6 +10,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
+  
   end
 
   def edit
