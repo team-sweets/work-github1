a9a7fb57b996da49fc5c95a64464abe3c1aae069
EC2 Default User <ec2-user@ip-172-31-25-157.ec2.internal>
2021-09-21 09:50:09 +0000

    customer_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_065256) do
+ActiveRecord::Schema.define(version: 2021_09_17_044823) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2021_09_18_065256) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "customer_id"
-    t.integer "product_id"
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_065256) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "kana_first_name", null: false
@@ -47,71 +45,60 @@ ActiveRecord::Schema.define(version: 2021_09_18_065256) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "phone_number", null: false
-    t.boolean "is_deleted", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "order_details", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
-    t.integer "quantity"
-    t.integer "production_status"
-    t.integer "purchased_price"
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.integer "quantity", null: false
+    t.integer "production_status", default: 0, null: false
+    t.integer "purchased_price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "address_name"
-    t.string "shipping_address"
-    t.string "shipping_postal_code"
-    t.integer "orders_status"
-    t.integer "postage"
-    t.integer "total_price"
-    t.integer "payment_method"
+    t.integer "customer_id", null: false
+    t.string "address_name", null: false
+    t.string "shipping_address", null: false
+    t.string "shipping_postal_code", null: false
+    t.integer "orders_status", default: 0, null: false
+    t.integer "postage", null: false
+    t.integer "total_price", null: false
+    t.integer "payment_method", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "image_id"
-    t.string "name"
-    t.text "description"
-    t.integer "tax_out_price"
-    t.boolean "sales_status"
+    t.integer "genre_id", null: false
+    t.string "image_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "tax_out_price", null: false
+    t.boolean "sales_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
-    t.string "address"
-    t.string "name"
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
