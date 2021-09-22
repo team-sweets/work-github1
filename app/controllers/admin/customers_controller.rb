478d10b750e689d07347a3ab8e2a728cb0da55c8
EC2 Default User <ec2-user@ip-172-31-14-149.ec2.internal>
2021-09-22 04:42:33 +0000

    formwith_ delete

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,24 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @customer = Customer.with_deleted.find
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    redirect_to admin_customer_path(customer.id)
+  end
+
+  private
+  def customer_params
+   params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number)
   end
 end
