ebced6416ce6d3a6aaab1b7904685d0642bcafcc
EC2 Default User <ec2-user@ip-172-31-14-149.ec2.internal>
2021-09-22 07:46:19 +0000

    customer_index_update

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 
  #enum sales_status: { on_sale: 0, sold_out: 1 }
 
+ validates :image, presence: true
  validates :name, presence: true
 	validates :description, presence: true
 	validates :tax_out_price, presence: true
