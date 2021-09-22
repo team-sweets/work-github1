c5d25f31f09f25166f5abc10a403d66a78a8ebec
EC2 Default User <ec2-user@ip-172-31-88-199.ec2.internal>
2021-09-22 04:42:18 +0000

    admin-products

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,10 @@
 class Admin::OrdersController < ApplicationController
   def top
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def show
+    
   end
 
   def update
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
+
+  enum orders_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
+
 end
