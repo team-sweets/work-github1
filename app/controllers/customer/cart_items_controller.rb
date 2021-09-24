671b8d6887067540387de7410bfa7ad80e9f8d02
EC2 Default User <ec2-user@ip-172-31-25-157.ec2.internal>
2021-09-24 03:53:12 +0000

    information

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,4 +1,8 @@
 class Customer::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :cart_item_check, only: [:create, :update]
+
+
 
   def index
     @cart_items = CartItem.all
@@ -7,35 +11,49 @@ class Customer::CartItemsController < ApplicationController
 
   def update
     cart_item = CartItem.find(params[:id])
-    cart_item.update(params_cart_item)
-    redirect_to cart_items_path
+    if cart_item.update(params_cart_item)
+     flash[:update] = "#{cart_item.product.name}を#{cart_item.quantity}個に変更しました!"
+     redirect_to cart_items_path
+    end
   end
 
   def create
     cart_item = CartItem.new(params_cart_item)
     cart_item.customer_id = current_customer.id
    if cart_item.save
-    flash[:notice] = "#{cart_item.product.name}をカートに追加しました"
+    flash[:notice] = "#{cart_item.product.name}をカートに#{cart_item.quantity}個追加しました!"
     redirect_to cart_items_path
    else
       @product = Product.find(params[:cart_item][:product_id])
       @cart_item = CartItem.new
-      flash[:alert] = "個数を選択してください"
-     render "customer/products/show"
    end
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to cart_items_path
+    if cart_item.destroy
+     flash[:destroy] = "#{cart_item.product.name}を削除しました!"
+     redirect_to cart_items_path
+    end
   end
 
   def destroy_all
     cart_item = CartItem.all
-    cart_item.destroy_all
-    redirect_to cart_items_path
+    if cart_item.destroy_all
+     flash[:destroy_all] = "カートを空にしました!"
+     redirect_to cart_items_path
+    end
   end
+
+  private
+
+  def cart_item_check
+    if params[:cart_item][:quantity].empty?
+      flash[:alert] = "※ 個数を選択してください"
+      redirect_to products_show_path((params[:cart_item][:product_id]).to_i)
+    end
+  end
+
 
   def params_cart_item
     params.require(:cart_item).permit(:quantity, :product_id)
