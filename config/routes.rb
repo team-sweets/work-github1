d23f16ea8fad45b50130d1cb647230891e7ad9e0
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-24 09:57:52 +0000

    header-layout/icon-changed

--- config/routes.rb
+++ config/routes.rb
@@ -44,28 +44,28 @@ Rails.application.routes.draw do
 
   namespace :admin do
 
-  get 'customers' => 'customers#index'
-  get 'customers/:id' => 'customers#show', as: 'customer'
-  get 'customers/:id/edit' => 'customers#edit', as: 'customer_edit'
-  patch 'customers/:id' => 'customers#update', as: 'customer_update'
+    get 'customers' => 'customers#index'
+    get 'customers/:id' => 'customers#show', as: 'customer'
+    get 'customers/:id/edit' => 'customers#edit', as: 'customer_edit'
+    patch 'customers/:id' => 'customers#update', as: 'customer_update'
 
-  get 'products' => 'products#index'
-  get	'products/new' => 'products#new'
-  post	'products'  => 'products#create'
-  get	'products/:id' => 'products#show', as: 'product'
-  patch	'products/:id' => 'products#update'
-  get	'products/:id/edit' => 'products#edit', as: 'product_edit'
+    get 'products' => 'products#index'
+    get	'products/new' => 'products#new'
+    post	'products'  => 'products#create'
+    get	'products/:id' => 'products#show', as: 'product'
+    patch	'products/:id' => 'products#update'
+    get	'products/:id/edit' => 'products#edit', as: 'product_edit'
 
-  get	'genres' => 'genres#index'
-  post 'genres' => 'genres#create'
-  get	'genres/:id/edit' => 'genres#edit', as: 'genres_edit'
-  patch	'genres/:id/edit' => 'genres#update'
+    get	'genres' => 'genres#index'
+    post 'genres' => 'genres#create'
+    get	'genres/:id/edit' => 'genres#edit', as: 'genres_edit'
+    patch	'genres/:id/edit' => 'genres#update'
 
-  get	'top' => 'orders#top'
-  get	'orders/:id' => 'orders#show'
-  patch	'orders/:id' => 'orders#update'
+    get	'top' => 'orders#top'
+    get	'orders/:id' => 'orders#show'
+    patch	'orders/:id' => 'orders#update'
 
-  patch	'order_details/:id' => 'order_details#update'
+    patch	'order_details/:id' => 'order_details#update'
 
  end
 
