336ed9513428c06b95e193635ce87975d6a8435d
EC2 Default User <ec2-user@ip-172-31-14-149.ec2.internal>
2021-09-25 07:11:03 +0000

    order-log

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,19 @@
 class Admin::SearchesController < ApplicationController
+
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+   private
+
+   def search_for(model, content, method)
+     if model == 'customer'
+        Customer.where('name LIKE ?', '%'+content+'%')
+     else model = 'product'
+        Product.where('title LIKE ?', '%'+content+'%')
+     end
+   end
 end
