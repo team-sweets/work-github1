c41fb48f7546111d306d383406ee8edba2c1d064
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-27 01:47:23 +0000

    change_error-message-language_to_english

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,9 +6,14 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    genre = Genre.new(genre_params)
-    genre.save
-    redirect_to  admin_genres_path
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      @genre = Genre.new
+      render :index
+    end
   end
 
   def edit
