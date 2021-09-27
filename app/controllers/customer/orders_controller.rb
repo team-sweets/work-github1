96a3d15e53dc1f12e3c6ef0442a9c621984d87d4
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-09-27 09:44:17 +0000

    layout-modified

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -48,9 +48,8 @@ class Customer::OrdersController < ApplicationController
        @order_detail.purchased_price = cart_item.product.add_tax_tax_out_price.to_i*cart_item.quantity
        @order_detail.save
      end
-   end
       @cart_items.destroy_all
-
+      redirect_to thanx_path
     @order.save
     #adress = ShippingAddress.new(shipping_parameter)
 
@@ -88,3 +87,4 @@ class Customer::OrdersController < ApplicationController
     redirect_to customers_cart_items_path if params[:id] == "log"
   end
   end
+end
